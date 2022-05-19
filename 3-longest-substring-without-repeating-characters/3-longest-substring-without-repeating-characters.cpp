1ac75f47dfed9e16af77c99c6c699a2489e382ac
Shivansh Sharma <shivanshsharma2012@gmail.com>
2022-05-19 22:33:51 +0530

    Time: 15 ms (72.41%), Space: 8.3 MB (59.16%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -14,35 +14,26 @@ public:
         end  = 1;
         v[s[start]].value = 0;
         int curr = 1, m = 1;
-        
-        for(auto x: s) {
-            cout<<v[x].value<<" ";
-        }
-        
-        cout<<endl;
+    
         
         while(end < n) {
             if(v[s[end]].value==-1) {
                 curr++;
-                cout<<"con1 "<<v[s[end]].value;
                 v[s[end]].value = end;
                 end++;
             } else {
                 int ind = v[s[end]].value;
                 if(ind>=start && ind<end) {
-                    cout<<"con2 "<<v[s[end]].value;
                     start = ind+1;
                     curr = end-start+1;
                     v[s[ind]].value = end;
                     end++;
                 } else {
-                    cout<<"con3 "<<v[s[end]].value;
                     v[s[end]].value = end;
                     end++;
                     curr++;
                 }
             }
-            cout<<endl;
             m = max(m, curr);
         }
         return m;
