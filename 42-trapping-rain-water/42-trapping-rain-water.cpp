e307102b971283a8d2d43acd782c574184e7a574
Shivansh Sharma <shivanshsharma2012@gmail.com>
2022-05-24 00:52:40 +0530

    Time: 25 ms (22.32%), Space: 20.5 MB (5.22%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,39 +1,47 @@
 class Solution {
 public:
+    vector<int> nextGreatest(vector<int> &h) {
+        int n = h.size();
+        vector<int> ans(n, -1);
+        int g;
+        g = h[n-1];
+        int i;
+        for(i=n-1;i>-1;i--) {
+            if(h[i]<g) {
+                ans[i] = g;
+            } else {
+                g = h[i];
+            }
+        }
+        return ans;
+    }
+    
+    vector<int> prevGreatest(vector<int> &h) {
+        int n = h.size(), i, g = h[0];
+        vector<int> ans(n, -1);
+        for(i = 0; i<n; i++) {
+            if(g>h[i]) {
+                ans[i] = g;
+            } else {
+                g = h[i];
+            }
+        }
+        return ans;
+    }
+    
     int trap(vector<int>& height) {
-        int i, n;
+        vector<int> ng = nextGreatest(height);
+        vector<int> pg = prevGreatest(height);
+        
+        int i, n, ans = 0;
         n = height.size();
-        vector<int> ng(n,-1);
-        vector<int> pg(n,-1);
-        int maxi = height[n-1];
         
-        for(i=n-2;i>-1;i--) {
-            if(height[i]<maxi) {
-                ng[i] = maxi;
-            } else {
-                maxi = height[i];
-            }
+        for(i=0; i<n; i++) {
+            if (ng[i]==-1 || pg[i]==-1) continue;
+            
+            int diff = min(ng[i], pg[i]) - height[i];
+            ans += diff;
         }
-        
-        maxi = height[0];
-        for(i=1;i<n;i++) {
-            if(height[i]<maxi) {
-                pg[i] = maxi;
-            } else {
-                maxi = height[i];
-            }
-        }
-        
-        int ans = 0;
-        int temp;
-        for(i=1; i<n-1; i++) {
-            if(pg[i]!=-1 && ng[i]!=-1) {
-                temp = min(pg[i], ng[i]);
-                ans += temp - height[i];
-            }    
-        }
-        
-        
         return ans;
     }
 };
