03050cb7c7b5b254e1896adacf2faa57c6e4a134
Shivansh Sharma <shivanshsharma2012@gmail.com>
2022-06-07 20:02:59 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (51.65%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -2,32 +2,32 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        map<char, char> m;
-        m[')'] = '(';
-        m[']'] = '[';
-        m['}'] = '{';
-        
-        int i, l;
-        l = s.length();
-        
-        if(l==1) return false;
-        
-        for (i = 0; i<l; i++) {
-            char ch = s[i];
-            if (ch=='}' || ch==')' || ch==']') {
-                if (st.size() != 0 && st.top()==m[ch]) {
-                    st.pop();
-                } else {
-                    return false;
-                }
+        if(s.length()==1) return false;
+        for(auto x: s) {
+            if(x=='(' || x=='[' || x=='{') {
+                st.push(x);
             } else {
-                st.push(ch);
+                if(x==')') {
+                    if(st.size()>0 && st.top()=='('){
+                        st.pop();
+                    } else return false;
+                }
+                
+                if(x=='}') {
+                    if(st.size()>0 && st.top()=='{'){
+                        st.pop();
+                    } else return false;
+                }
+                
+                if(x==']') {
+                    if(st.size()>0 &&  st.top()=='['){
+                        st.pop();
+                    } else return false;
+                }
             }
         }
         
-        if(st.size()==0) {
-            return true;
-        }
+        if(st.size()==0)  return true;
         return false;
     }
 };
