1519d68c331c5ed2de299d8c80cd63241883f098
Shivansh Sharma <shivanshsharma2012@gmail.com>
2022-07-02 13:06:37 +0530

    Time: 0 ms, Memory: 7.1 MB - LeetHub

---
+++ subsets/subsets.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> v;
+        ans.push_back(v);
+        
+        for (auto i: nums) {
+            int n = ans.size();
+            for(int j = 0; j<n; j++) {
+                vector<int> temp = ans[j];
+                temp.push_back(i);
+                ans.push_back(temp);
+            }
+        }
+        return ans;
+    }
+};
