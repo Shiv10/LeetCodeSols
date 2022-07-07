e24ab15a0a046098d83b80ec96a7f724a26740d4
Shivansh Sharma <shivanshsharma2012@gmail.com>
2022-07-07 14:57:22 +0530

    Time: 296 ms (5.16%), Space: 97.3 MB (5.53%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -3,22 +3,23 @@ public:
     int maxProfit(vector<int>& prices) {
         int n, i, maxe;
         n = prices.size();
-        vector<int> ng(n, -1);
-        
+        vector<int> ng(n);
+        ng[n-1] = -1;
         maxe = prices[n-1];
-        for(i=n-2;i>-1;i--) {
-            if (prices[i]<maxe) {
+        for (i = n-2; i>-1; i--) {
+            if (prices[i]<=maxe) {
                 ng[i] = maxe;
             } else {
+                ng[i] = -1;
                 maxe = prices[i];
             }
-        };
+        }
         
         int ans = 0;
-        for(i = 0; i<n; i++) {
-            if(ng[i]==-1) continue;
-            
-            ans = max(ans, ng[i]-prices[i]);
+        for (i = 0; i<n; i++) {
+            if (ng[i]!=-1) {
+                ans = max(ans, ng[i] - prices[i]);
+            }
         }
         return ans;
     }
