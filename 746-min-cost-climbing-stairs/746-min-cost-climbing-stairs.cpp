894b64430cca91f809c5762c566730bdbd57c0fc
Shivansh Sharma <shivanshsharma2012@gmail.com>
2022-07-15 21:38:39 +0530

    Time: 15 ms (27.62%), Space: 14 MB (9.41%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    
-    int minCostHelper(vector<int>& cost, int curr, int n, vector<int> &memo) {
-        if(curr>=n) return 0;
+    int solve(vector<int> &cost, int i, vector<int> &memo) {
+        if (i>cost.size()-1) {
+            return 0;
+        }
         
-        if(memo[curr]!=-1) return memo[curr];
+        if(memo[i]!=-1) return memo[i];
         
-        int minCost = INT_MAX;
+        int ans = 0;
+        ans = cost[i] + min(solve(cost, i+1, memo), solve(cost, i+2, memo));
         
-        minCost = cost[curr] + min({ minCostHelper(cost, curr+1, n, memo), minCostHelper(cost, curr+2, n, memo) });
-        
-        return memo[curr] = minCost;
+        return memo[i] = ans;
     }
-    
     int minCostClimbingStairs(vector<int>& cost) {
-        int a1, a2, n;
-        n = cost.size();
-        
-        vector<int> memo(n+1, -1);
-        
-        a1 = minCostHelper(cost, 0, n, memo);
-        a2 = minCostHelper(cost, 1, n, memo);
-        
-        return min(a1, a2);
+        int n = cost.size();
+        vector<int> memo(n, -1);
+        int ans = min(solve(cost, 0, memo), solve(cost, 1, memo));
+        return ans;
     }
 };
